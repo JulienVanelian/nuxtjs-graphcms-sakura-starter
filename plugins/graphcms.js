46801ed3f3f04b092edc64108ebb2e66b33e4d34
Distrikt64 <8573006+JulienVanelian@users.noreply.github.com>
2022-05-17 13:26:52 +0200

    Updated dependencies, removed useless line of code

--- plugins/graphcms.js
+++ plugins/graphcms.js
@@ -1,7 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
 
-const url = process.env.GRAPHCMS_ENDPOINT
-
 export default (_, inject) => {
   inject('graphcms', new GraphQLClient(process.env.graphcmsEndpoint))
 }
